45bef7d936f9bfdb2e1835e0fa03ea082d5c96ad
William Waites <wwaites@tardis.ed.ac.uk>
2014-06-16 19:49:08 +0100

    remove possibly extraneous reference to Simulator and mois.core removed by rhz merge

--- MOISWebinterface/src/main/scala/ScalatraBootstrap.scala
+++ MOISWebinterface/src/main/scala/ScalatraBootstrap.scala
@@ -7,8 +7,8 @@ import akka.actor._
 import ed.mois.web._
 import org.scalatra._
 import javax.servlet.ServletContext
-import ed.mois.core.comm._
-import ed.mois.core.sim.Simulator
+//import ed.mois.core.comm._
+//import ed.mois.core.sim.Simulator
 
 class ScalatraBootstrap extends LifeCycle {
   //val simulator = Simulator.system.actorOf(Props(new SimulatorRefImpl1(25)), name = "simulator")
@@ -20,7 +20,10 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new IMSControlServlet, "/*")
   }
 
+/*
+ * XXX Where has Simulator gone? Disappeared after merging of rhz tree
   override def destroy(context: ServletContext) {
     Simulator.system.shutdown()
   }
+  */
 }
